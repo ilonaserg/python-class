fd1ba1a3280f01dcd2bf00d52343be278de798f5
ilonaserg <asteria2009scorp@gmail.com>
2024-05-21 11:38:19 -0500

    added new file

---
+++ class2/test_file.py
@@ -0,0 +1,131 @@
+#!workspace/bin/python
+"""
+    This is a documentation
+"""
+# A  =  5
+# B  = 10
+# C = A + B
+
+# print(C)
+
+
+
+
+
+# FRUIT = "apple"
+
+# print(dir(FRUIT))
+
+
+# print(type(FRUIT))
+
+# print(FRUIT.capitalize())
+# print(FRUIT.isdigit())
+# print(FRUIT.upper())
+# print(FRUIT.lower())
+
+
+# RESPONSE = input("Please enter your name: ")
+
+# print(RESPONSE.strip())
+
+# print(RESPONSE.lstrip())
+
+# print(RESPONSE.rstrip())
+
+
+NUMBER1  = 5
+NUMBER2  = 10
+
+print(type(NUMBER1))
+print(dir(NUMBER1))
+
+RESPONSE1 = NUMBER1  + NUMBER2
+
+NUMBER1  * NUMBER2
+
+NUMBER2  / NUMBER1
+
+NUMBER2  - NUMBER1
+
+# # Flots
+# NUMBER4 = 2.0 
+
+# print(type(NUMBER4)) 
+
+# List
+# FRUIT = ["apple", "pear", "apple, ""apricot", "orange"]
+
+# print(FRUIT.count("apple"))
+
+# print(type(FRUIT))
+
+# print(FRUIT)
+
+# print(dir(FRUIT))
+
+# FRUIT.append("melon")
+
+
+
+# print(FRUIT)
+
+# FRUIT.clear()
+
+# print(FRUIT)
+
+# Tuple
+# FRUIT = ("apple", "pear",)
+# print(dir(FRUIT))
+# print(type(FRUIT))
+
+# Maps also dictionary 
+
+# FRUIT = {
+#     "big": "apple",
+#     "sweet":  "melon"
+# }
+
+# print(dir(FRUIT))
+# print(type(FRUIT))
+
+# print(FRUIT["sweet"])
+
+# print(FRUIT.values())
+# print(FRUIT.keys())
+
+# Converting thr data
+# List()  allows to convert data to list
+# str()   allows to convert int to a string
+# float()
+
+# NUMBER1 = 2
+
+# NUMBER2 = float(NUMBER1)
+
+# print(type(NUMBER2))
+# print(NUMBER2)
+
+# NUMBER1 = 10
+
+# NUMBER2 = str(NUMBER1)
+
+# print(type(NUMBER2))
+
+# FRUIT = "apple"
+
+
+# print(type(FRUIT))
+
+# LISTED_FRUIT = list(FRUIT)
+
+# print(LISTED_FRUIT)
+# print(type(LISTED_FRUIT))
+
+
+# GREETENGS = "Hello"
+# RESPONSE = input("Please enter your name: ")
+
+# print(GREETENGS + " " + RESPONSE)
+
+
