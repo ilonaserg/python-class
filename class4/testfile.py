9dd736b65374c38b86fdf2ffc0069af341116dc2
ilonaserg <asteria2009scorp@gmail.com>
2024-05-24 20:18:35 -0500

    added class4

---
+++ class4/testfile.py
@@ -0,0 +1,118 @@
+#!workspace/bin/python
+import time
+
+# FRUIT = ["apple", "pear", "banana"]
+
+# for items in FRUIT:
+#     print(items)
+
+# for items in range(10):
+#     print(items + 1)
+
+
+# FILE1 = open("/etc/passwd")
+
+# for lines in FILE1:
+#     time.sleep(0.1)
+#     print(lines.strip())
+
+# FILE1.close()
+
+
+# import string 
+
+# # print(dir(string))
+
+# for letters in string.ascii_lowercase:
+#     print(letters)
+
+# for letters in string.ascii_uppercase:
+#     print(letters)
+
+USERLIST = ["john", "smith", "3121233213", "a@gmail.com"]
+
+# for items in USERLIST:
+#     print(items)
+
+# for items in USERLIST:
+#     if items.isnumeric():
+#         print(items)
+
+# for items in USERLIST:
+#     if items.isalpha():
+#         print(items)
+
+# for items in USERLIST:
+#     if items.endswith(".com"):
+#         print(items)
+
+# for items in USERLIST:
+#     if items.isalpha():
+#         print(items.capitalize)
+
+# for items in USERLIST:
+#     if items == "smith":
+#         break
+
+#     print(items)
+
+# TOTAL_PRODUCTS = ["bread", "meat", "cheese", "carrot", "rice", "potato"]
+# WE_HAVE = ["bread", "meat"]
+
+# for items in TOTAL_PRODUCTS:
+#     if items not in WE_HAVE:
+#         print("We need to buy %s" %items)
+
+# LIST_OF_NUMBERS = [1,2,3,4,5,6,7]
+
+# for items in LIST_OF_NUMBERS:
+#     #find even number
+#     if items % 2 == 0:
+#         print(items)
+
+# for items in LIST_OF_NUMBERS:
+#     #find odd number
+#     if items % 2 != 0:
+#         print(items)       
+
+# NUMBER = 0
+
+
+# while  NUMBER != 5:
+#     time.sleep(0.2)
+#     NUMBER = NUMBER +1
+#     print(NUMBER)
+
+
+
+
+
+# RESPONSE = int()
+# while True:
+#     try:
+#         RESPONSE = int(input("When were you born: "))
+#         if RESPONSE >= 2025:
+#             print("You are not born yet")
+
+#         elif RESPONSE >= 2000 and RESPONSE <= 2024:
+#             print("You were born in 21st century")
+
+#         elif RESPONSE <= 1999 and RESPONSE >= 1990:
+#             print("You are in your 30th")
+
+#         elif RESPONSE <= 1989 and RESPONSE >= 1980:
+#             print("You are in your 40th")
+
+#         else:
+#             print("try again")
+
+#     except ValueError:
+#         print("Only digits are accepted. Please type number e.g 2000")
+
+
+ANSWER = "no"
+
+while ANSWER != "yes":
+    ANSWER = str(input("Are you crazy?:"))
+    if ANSWER == "yes":
+        print("I knew that")
