5f68f9c1255c69b3bddb23c3b72d74e08c04e17c
ilonaserg <asteria2009scorp@gmail.com>
2024-06-05 20:53:27 -0500

    added lambda function to class8

--- evolve-cyber/class7/s3/bucket_tagger.py
+++ evolve-cyber/class7/s3/bucket_tagger.py
@@ -1,67 +1,56 @@
 # import boto3 library
-import boto3
-
-# Initialize boto3 s3 client
-s3 = boto3.client("s3")
-
-# Define variable for a region
-REGION = "us-east-2"
-
-# Create empty list
-BUCKET_LIST = []
+import boto3 
+import json
 
 
-TAGS = {
-    'TagSet': [
-        {
-            'Key': 'envi',
-            'Value': 'dev'
-        },
-        {
-            'Key': 'managedBy',
-            'Value': 'terraform'
-        },
-        {
-            'Key': 'python',
-            'Value': 'class'
-        },
-    ]
-}
+def lambda_handler(event, context):
+    # Initialize boto3 s3 client 
+    s3 = boto3.client("s3")
 
+    # Define variable for a region
+    REGION = "eu-west-2"
 
+    # Create empty list
+    BUCKET_LIST = []
 
-# print (type(s3))
-# print (type(boto3))
-# print (dir(boto3))
-# print(dir(boto3.s3))
+    TAGS = {
+        'TagSet': [
+            {
+                'Key': 'envi',
+                'Value': 'dev'
+            },
+            {
+                'Key': 'managedBy',
+                'Value': 'terraform'
+            },
+            {
+                'Key': 'python',
+                'Value': 'class'
+            },
+        ]
+    }
 
+    # List S3 buckets and save as "response"
+    response = s3.list_buckets()
 
-# List S3 buckets and save as "response"
-response = s3.list_buckets()
-
-# iterate thru response list and print bucketname
-for bucket in response["Buckets"]:
-    BUCKET_LIST.append(bucket["Name"])
-
-
-for bucket in BUCKET_LIST:
-    # Find bucket location
-    bucket_location = s3.get_bucket_location(Bucket=bucket) 
-    bucket_region = bucket_location["LocationConstraint"]
-    print("This bucket %s is located at %s" % (bucket, bucket_region))
-
-    # if bucket is in region 
-    if bucket_region == REGION:
-        # print to the screen
-        print("The bucket %s is located in %s" % (bucket, bucket_region))
-        # add tags
-        response = s3.put_bucket_tagging(Bucket=bucket,Tagging=TAGS)
-        print("The following buckets %s are tagged" % bucket)
+    # iterate thru response list and print bucketname
+    for bucket in response["Buckets"]:
+        BUCKET_LIST.append(bucket["Name"])
 
 
 
+    for bucket in BUCKET_LIST:
+        # Find bucket location
+        bucket_location = s3.get_bucket_location(Bucket=bucket) 
+        bucket_region = bucket_location["LocationConstraint"]
 
-
+        # if bucket is in region
+        if bucket_region == REGION:
+            # print to the screent
+            print("The bucket %s is located in %s" % (bucket, bucket_region))
+            # add tags
+            response = s3.put_bucket_tagging(Bucket=bucket,Tagging=TAGS)
+            print("The following buckets %s are tagged" % bucket)
 
 
  
